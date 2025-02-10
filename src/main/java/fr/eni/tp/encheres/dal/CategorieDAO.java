2b436f0ce29ea0e0684a59d1f1007f190ebeae35
Gildas <herbreteau.gildas@hotmail.fr>
2025-02-10 17:09:50 +0100

    Nouvelle vente3

--- src/main/java/fr/eni/tp/encheres/dal/CategorieDAO.java
+++ src/main/java/fr/eni/tp/encheres/dal/CategorieDAO.java
@@ -1,9 +1,13 @@
 package fr.eni.tp.encheres.dal;
 
+import java.util.List;
+
 import fr.eni.tp.encheres.bo.Categorie;
 
 public interface CategorieDAO {
 
 	Categorie getCategorie(int noCategorie);
+
+	List<Categorie> getCategories();
 
 }
